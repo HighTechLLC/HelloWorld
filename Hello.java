cb9207cf083c5068f40459788d6b297bea7e5abd
TANER GUR <tanergur@TANERs-MacBook-Pro.local>
2020-12-23 00:11:56 -0500

    added HelloWorld program

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package jenkins.javawork;
+
+public class Hello {
+
+	public static void main (String [] args) {
+		
+		for (int i=1; i<=10; i++) {				
+			System.out.println("Hello World ..." + i);
+			System.out.println("SUCCESS ..." + i);
+			System.out.println("OK ..." + i);
+		}		
+	}
+}
